89df98d45b6df348d648f6198a2e18685ba73c1a
Rafa Gómez <rgomezcasas@gmail.com>
2024-02-19 11:24:08 +0100

    feat(4-1): add example

--- 04-generate_queues_automatically/1-configure_programatically/1-configure.sh
+++ 04-generate_queues_automatically/1-configure_programatically/1-configure.sh
@@ -4,20 +4,20 @@ aws events \
 	--region us-east-1 \
 	create-event-bus --name codely.domain_events
 
-# Queue
-aws sqs \
-	--endpoint-url http://localhost:4566 \
-	--region us-east-1 \
-	create-queue --queue-name send_welcome_email_on_user_registered
-
-# Subscription
+# Rules
 aws events \
 	--endpoint-url http://localhost:4566 \
 	--region us-east-1 \
 	put-rule --name rule-user_registered \
 	         --event-bus-name codely.domain_events \
 	         --event-pattern '{"detail-type": ["user.registered"]}'
+# Queues
+aws sqs \
+	--endpoint-url http://localhost:4566 \
+	--region us-east-1 \
+	create-queue --queue-name send_welcome_email_on_user_registered
 
+# Targets
 aws events put-targets --endpoint-url http://localhost:4566 \
     --region us-east-1 \
     --event-bus-name codely.domain_events \
